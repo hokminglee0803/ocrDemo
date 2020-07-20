69f0ceeaf1d52c8b39854d82940d53d7530e0563
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2020-07-21 02:06:54 +0800

    Update Get Image By Camera Feature

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View ,Button,  ActivityIndicator,StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
 import axios from "axios";
+import * as ImagePicker from 'expo-image-picker';
 
-class App extends Component{
+class App extends React.Component{
 
   ocr_api_endpoint = `https://ocr-ypkwsijlva-de.a.run.app/ocr`;
 
@@ -11,28 +13,48 @@ class App extends Component{
     super(props);
     this.state = {
         imageFile: null,
-        imagePreview:'',
         result: '',
+        image: null,
+        loading: false
     }
   }
 
-      // Image Preview Handler
-      handleImagePreview = (e) => {
-        let imageBase64 = URL.createObjectURL(e.target.files[0])
-        let imageFiles = e.target.files[0];
+  componentDidMount() {
+    this.getCameraRollPermissionAsync();
+    this.getCameraPermissionAsync();
+  }
+      getCameraRollPermissionAsync = async () => {
+      if (Constants.platform.ios)  {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      };
 
-        this.setState({
-            imagePreview: imageBase64,
-            imageFile: imageFiles,
-        })
-    }
+      getCameraPermissionAsync = async () => {
+        if (Constants.platform.ios)  {
+          const { status } = await Permissions.askAsync(Permissions.CAMERA);
+            if (status !== 'granted') {
+              alert('Sorry, we need camera roll permissions to make this work!');
+            }
+          }
+        };
 
-    handleSubmitFile = () => {
+    handleSubmitFile = async() => {
+      if (this.state.image !== null){
+      
+        this.setState({loading: true});
+        const img = document.getElementById('id')
 
-      if (this.state.imageFile !== null){
+      fetch(img.src)
+      .then(res => res.blob())
+      .then(blob => {
+        const file = new File([blob], 'dot.png', blob)
+        console.log(file)
 
-          let formData = new FormData();
-          formData.append('file', this.state.imageFile);
+        let formData = new FormData();
+          formData.append('file',file );
           axios.post(
               this.ocr_api_endpoint,
               formData,
@@ -45,45 +67,84 @@ class App extends Component{
           .then(res => {
             this.setState({
               result: res.data,
+              loading: false
             })
           })
           .catch(err => {
-              console.log(err);
+            this.setState({
+              result: err,
+              loading: false
+            })
           })
+      })
       }
+      
   }
+
+  pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result });
+      }
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  pickImageFromCamera = async () => {
+    try {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        base64 : true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result });
+      }
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
     
     
     render() { 
+      let { image,result,loading} = this.state;
       return (
-        <View style={styles.container}>
-        <Text>OCR Demo Application</Text>
-        <StatusBar style="auto" />
-        <img src={this.state.imagePreview} alt="image preview"/>  
-        <div>
-          <input
-              type="file"
-              onChange={this.handleImagePreview}
-          />
-        </div>
-        <div>
-          <input type="submit" onClick={this.handleSubmitFile} value="Submit"/>
-        </div>
-        <div>
-          {this.state.result}
-        </div>
-      </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>OCR Demo Application</Text>
+          <StatusBar style="auto" /> 
+          <Button title="Pick an image from Camera roll" onPress={this.pickImage}  />
+          <Button title="Pick an image from Camera" onPress={this.pickImageFromCamera} />
+          {image && 
+          <React.Fragment>
+            <Button title="Convert" onPress={() => this.handleSubmitFile()} />
+            <img id="id" src={image.uri} style={{ width: 200, height: 200 }}/>
+          </React.Fragment>
+          }
+          <ActivityIndicator
+            animating={loading} />
+          <View>
+            {
+              result !== ''?
+              <Text> {result}</Text>
+                :null
+            }
+          </View>
+        </View>
       );
     }
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
